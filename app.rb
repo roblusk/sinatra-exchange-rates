68fc6d0b69cea1cd2243ff81de51014c7933ec61
roblusk <130242918+roblusk@users.noreply.github.com>
2024-04-12 05:45:05 +0000

    finish from_currency page

--- app.rb
+++ app.rb
@@ -2,16 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 require "http"
 
-get("/") do
+def get_currencies()
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
   raw_response = HTTP.get(api_url)
   parsed_response = JSON.parse(raw_response)
-  @currencies = parsed_response.fetch("currencies")
+  return parsed_response.fetch("currencies")
+end
+
+get("/") do
+  @currencies = get_currencies()
   erb(:home)
 end
 
 get("/:from_currency") do
-  
+  @currencies = get_currencies()
+  @from_currency = params.fetch("from_currency")
+  erb(:from_currency)
 end
 
 get("/:from_currency/:to_currency") do
